543b8ebf7762e8212d71f744c6bcba2832201641
Simon Baars <simon.mailadres@gmail.com>
2019-07-23 09:42:17 +0200

    Fixed Validate corpus

--- src/main/java/com/simonbaars/githubjavacorpus/ValidateCorpus.java
+++ src/main/java/com/simonbaars/githubjavacorpus/ValidateCorpus.java
@@ -1,18 +1,23 @@
 package com.simonbaars.githubjavacorpus;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.simonbaars.clonerefactor.util.FileUtils;
-import com.simonbaars.clonerefactor.util.SavePaths;
+import com.simonbaars.githubjavacorpus.utils.DoesFileOperations;
+import com.simonbaars.githubjavacorpus.utils.SavePaths;
 
-public class ValidateCorpus {
+public class ValidateCorpus implements DoesFileOperations {
 
 	public static void main(String[] args) throws IOException {
-		String file = FileUtils.getFileAsString(SavePaths.getApplicationDataFolder()+"projects.txt");
+		new ValidateCorpus().validateCorpus();
+	}
+
+	private void validateCorpus() throws IOException, FileNotFoundException {
+		String file = getFileAsString(SavePaths.getApplicationDataFolder()+"projects.txt");
 		FileOutputStream fos = new FileOutputStream(new File(SavePaths.getApplicationDataFolder()+"filtered_projects.txt"));
 		for(String s : file.split("\n")) {
 			int pom = doRequest("https://raw.githubusercontent.com"+s+"/master/pom.xml"), sourceFolder = doRequest("https://github.com"+s+"/tree/master/src/main/java");
