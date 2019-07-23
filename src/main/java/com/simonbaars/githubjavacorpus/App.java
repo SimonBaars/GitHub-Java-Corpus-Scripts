5e04a9dfeb50d28aa3f6df58a5d847c27310113e
Simon Baars <simon.mailadres@gmail.com>
2019-07-23 09:34:12 +0200

    Intial Commit

---
+++ src/main/java/com/simonbaars/githubjavacorpus/App.java
@@ -0,0 +1,13 @@
+package com.simonbaars.githubjavacorpus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
