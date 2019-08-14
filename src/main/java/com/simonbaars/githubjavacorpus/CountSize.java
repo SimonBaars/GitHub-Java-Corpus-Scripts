83cbf2af83e33cf54eb1e8f052ef6dc97ec9c2d2
Simon Baars <simon.mailadres@gmail.com>
2019-08-14 20:14:58 +0200

    Finished the implementation

--- src/main/java/com/simonbaars/githubjavacorpus/CountSize.java
+++ src/main/java/com/simonbaars/githubjavacorpus/CountSize.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.utils.SourceRoot;
 import com.github.javaparser.utils.SourceRoot.Callback.Result;
 import com.simonbaars.githubjavacorpus.utils.SavePaths;
@@ -19,15 +17,22 @@ public class CountSize {
 	public static void main(String[] args) throws IOException {
         final ParserConfiguration config = new ParserConfiguration()
     			.setLexicalPreservationEnabled(false)
-    			.setStoreTokens(false);
-        int total = 0;
+    			.setStoreTokens(true);
         File[] corpusFiles = new File(SavePaths.getApplicationDataFolder()+"git").listFiles();
-        
-		new SourceRoot(Paths.get(corpusFiles[i]+"src/main/java/")).parse("", config, (Path localPath, Path absolutePath, ParseResult<CompilationUnit> result) -> {
-			if(result.getResult().isPresent()) {
-				total+=result.getResult().get().getRange().get().end.line+1;
-			}
-			return Result.DONT_SAVE;
-		});
+        AtomicInteger ordinal = new AtomicInteger(0);
+        for(File f : corpusFiles) {
+        	Path p = Paths.get(f+"/src/main/java/");
+        	if(p.toFile().isDirectory()) {
+				new SourceRoot(Paths.get(f+"/src/main/java/")).parse("", config, (Path localPath, Path absolutePath, ParseResult<CompilationUnit> result) -> {
+					if(result.getResult().isPresent() && result.getResult().get().getRange().isPresent()) {
+						int size = result.getResult().get().getRange().get().end.line+1;
+						ordinal.addAndGet(size);
+						System.out.println(f.getName()+"\t"+size);
+					}
+					return Result.DONT_SAVE;
+				});
+        	}
+        }
+        System.out.println("Combined size = "+ordinal.get());
 	}
 }
